c769f6b873965d01685441f8eb2016890152e11b
Nicholas <njhylands@gmail.com>
2018-05-12 15:36:02 -0400

    Testing out different screen navigation

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import { ListView, View, Platform, Text, TextInput, KeyboardAvoidingView,ScrollView} from 'react-native';
+//import { ListView, View, Platform, Text, TextInput, KeyboardAvoidingView,ScrollView} from 'react-native';
+import { Button, View, Text } from 'react-native';
 import ReactNative from 'react-native';
 import * as firebase from 'firebase';
-
-const StatusBar = require('./components/StatusBar');
-const ActionButton = require('./components/ActionButton');
-const ListItem = require('./components/ListItem');
-
-const styles = require('./styles.js')
+import { createStackNavigator , } from 'react-navigation';
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -16,97 +12,47 @@ const firebaseConfig = {
   databaseURL: "https://codingchallengetest.firebaseio.com/",
   storageBucket: "<your-storage-bucket>",
 };
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+const RootStack = createStackNavigator({
+  Chat: ChatScreen,
+  Contacts: ContactsListScreen,
+})
 
+const styles = require('./styles.js');
 
-export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    
-    console.ignoredYellowBox = ['Setting a timer']; // keep getting this error on android, apparently it's not that important.
-    //console.log("testing");
-    this.state = {
-      dataSource: new ListView.DataSource( { rowHasChanged: (row1, row2) => row1 !== row2, } ),
-      chatTextToSend: ""
-    };
-
-    //set up a way for us to access the database.
-    this.itemsRef = firebaseApp.database().ref();
-  }
-
-  componentDidMount() {
-    /*setInterval( () => {
-      this.setState({
-        curTime : new Date().toLocaleString()
-      })
-    },1000)*/
-    this.listenForItems(this.itemsRef);
-    //this.setState({ dataSource: this.state.dataSource.cloneWithRows([{ title: "Pizza" }]) })
-  }
-
-  listenForItems(itemsRef) {
-    itemsRef.on('value', (snap) => {
-      
-      // get children as an array  
-      var items = [];
-      snap.forEach((child) => {
-        // we're iterating through each entry at the given database url, and we're adding it to the array of items 
-        items.push({ title: child.val(), _key: child.key });
-        //console.log(child.val());
-      });
-      
-      // now we're going to add the items to the dataSource ListView.
-      this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(items)
-      });
-
-    });
-  }
-
-  _addItem() {
-    //console.log('Sent a Message.');
-
-    this.itemsRef.push("[" + Date().toLocaleString() + "]: " + this.state.chatTextToSend);
-
-    this.state.chatTextToSend = "";
-  }
-
-  _renderItem(item){
-    return (
-      <ListItem item={item} onpress={() => {}}/>
-    );
-  }
-
-
+class ChatScreen extends Component{
+  static navigationOptions = {
+    title: 'Chat Screen',
+  };
   render() {
     return (
-      
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <StatusBar title="Chat With: "/>
-        <ScrollView ref={ref => this.scrollView = ref} onContentSizeChange={(contentWidth, contentHeight)=>{this.scrollView.scrollToEnd({animated: true});}}>
-          
-          <ListView dataSource={this.state.dataSource} renderRow={this._renderItem.bind(this)} style={styles.listview}/>
-        </ScrollView>
-        <TextInput 
-          style={
-            {
-              height: 40,
-              borderColor: 'gray', 
-              borderWidth: 1,
-              paddingLeft: 16,
-            }
-          }
-          placeholder="send a message"
-          onChangeText={(chatTextToSend) => this.setState({chatTextToSend})}
-          value={this.state.chatTextToSend}
-        />
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Chat Screen</Text>
+      </View>
+    );
+  }
+}
 
-        <ActionButton title="Add" onPress={this._addItem.bind(this)}>
-        </ActionButton>
+class ContactsListScreen extends Component{
+  static navigationOptions = {
+    title: 'Contacts List',
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>List of Contacts</Text>
+      </View>
+    );
+  }
+}
 
-      </KeyboardAvoidingView>
+//Main component with main render loop
+export default class App extends Component {
+  render() {
+    return (
+      <RootStack/>
     );
   }
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Simple chat app for a coding challenge.
 
-I'll be shutting down access to the database until user authentication is added. Contact me if you'd like to see it working.
+https://trello.com/b/aYKc3nS0/chatappdesign
+
+
